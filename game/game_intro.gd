a7c3145e22120d33d376570e7ff36887eaa4d19b
Lovely Shane Ong <134838637+xienshane@users.noreply.github.com>
2025-10-30 13:02:35 +0800

    added some house materials

--- game/game_intro.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$AnimationPlayer.play("cutscene")
-	await get_tree().create_timer(19, false).timeout
-	get_tree().change_scene_to_file("res://game/level.tscn")
