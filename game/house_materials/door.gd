157af68285ba83b5b48e9f5a8d806ce8702a1736
Luis <chrisluisfern1@gmail.com>
2025-10-31 23:24:50 +0800

    head bobbing and soundfx

--- game/house_materials/door.gd
+++ game/house_materials/door.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
 var opened = false
+@onready var door_open: AudioStreamPlayer3D = $Door_Open
+@onready var door_close: AudioStreamPlayer3D = $Door_Close
 
 func toggle_door():
 	if $AnimationPlayer.current_animation != "open" and $AnimationPlayer.current_animation != "close":
 		opened = !opened
 		if !opened:
 			$AnimationPlayer.play("close")
+			door_close.play()
 		if opened:
 			$AnimationPlayer.play("open") 
+			door_open.play()
