aa8e603f84fa9192565fedd4c9eaa0ed1ecc22f8
YelzyDy <zendymariel.dy@gmail.com>
2025-11-04 18:32:45 +0800

    monster ai- a bit smarter

--- game/destination.gd
+++ game/destination.gd
@@ -1,4 +1,5 @@
 extends Node3D
 func enter_trigger(body) :
-	if body.name == "enemy" and body.destination == self:
+	if body.name == "ghost" and body.destination == self:
 		body.pick_destination(body.destination_value)
+		print("DV: " + body.destination_value)
--- scripts/ghost.gd
+++ scripts/ghost.gd
@@ -1,4 +1,6 @@
 extends CharacterBody3D
+@onready var agent =$NavigationAgent3D
+var arrival_distance: float = 1.2
 @export var patrol_destinations: Array[Node3D]
 @onready var player = get_tree().current_scene.get_node("player")
 var speed = 3.0
@@ -17,6 +19,7 @@ func _process(delta: float) -> void:
 		var look_dir = lerp_angle(deg_to_rad(global_rotation_degrees.y), atan2(-velocity.x, -velocity.z), 0.5)
 		global_rotation_degrees.y = rad_to_deg(look_dir)
 		update_target_location()
+
 		
 func _physics_process(_delta: float) -> void:
 	if destination != null:
@@ -25,6 +28,9 @@ func _physics_process(_delta: float) -> void:
 		var new_velocity = (next_location - current_location).normalized() * speed
 		velocity = velocity.move_toward(new_velocity, 0.25)
 		move_and_slide()
+	if agent.is_navigation_finished():
+		pick_destination(destination_value)
+
 		
 func pick_destination(dont_choose = null):
 	var num = rng.randi_range(0, patrol_destinations.size() - 1)
